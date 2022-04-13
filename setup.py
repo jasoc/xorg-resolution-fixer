be233021ecea37c09fbd16d1ab0ab5c251cd3007
jasoc <paridegiunta@gmail.com>
2022-04-13 19:04:21 +0200

    Removed useless comments

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), f'{fname}.md')).read()
 
 setup(
     name = "xorg_resolution_fixer",
-    version = "0.0.5",
+    version = "1.0.0",
     author = "Paride Giunta",
     author_email = "paridegiunta@gmail.com",
-    description = (""),
+    description = ("A python module that uses xrandr and cvt to force a specific resolution"),
     license = "MIT",
     keywords = "xorg resolution xrandr",
     entry_points = {
