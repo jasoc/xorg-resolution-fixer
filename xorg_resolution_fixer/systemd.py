4aafa2962a74c2662cb99c4429f987a203f1f586
Jasoc <paridegiunta@gmail.com>
2025-08-26 16:50:45 +0200

    Refactor getService function to accept xrf_path parameter and update service creation logic

--- xorg_resolution_fixer/systemd.py
+++ xorg_resolution_fixer/systemd.py
@@ -1,5 +1,5 @@
 import os
-
+from .xrandr import run
 
 xrf_service_name = 'xorg-resolution-fixer.service'
 
@@ -8,7 +8,7 @@ def defaultSystemdPath(user):
     return f'/home/{user}/.config/systemd/user'
 
 
-def getService(height, width, refresh_rate, display):
+def getService(height, width, refresh_rate, display, xrf_path):
     return f'''
 [Unit]
 Description=Xorg resolution fixer
@@ -17,7 +17,7 @@ After=graphical-session.service
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/xrf -s -e {height} -w {width} -r {refresh_rate} -d {display}
+ExecStart={xrf_path} -s -e {height} -w {width} -r {refresh_rate} -d {display}
 
 [Install]
 WantedBy=graphical-session.target
@@ -25,6 +25,14 @@ WantedBy=graphical-session.target
 
 
 def createService(height, width, refresh_rate, display, user):
+    xrf_path = '/usr/local/bin/xrf'
+    if not os.path.isfile(xrf_path):
+        xrf_path = '/usr/bin/xrf'
+        if not os.path.isfile(xrf_path):
+            print("[ERROR] xrf binary not found in /usr/local/bin/xrf or /usr/bin/xrf. Cannot create systemd service.")
+            exit(1)
     os.makedirs(defaultSystemdPath(user), exist_ok=True)
     with open(f'{defaultSystemdPath(user)}/{xrf_service_name}', 'w') as f:
-        f.write(getService(height, width, refresh_rate, display))
+        f.write(getService(height, width, refresh_rate, display, xrf_path))
+    run("systemctl --user daemon-reload")
+    run(f"systemctl --user enable --now {xrf_service_name}")
